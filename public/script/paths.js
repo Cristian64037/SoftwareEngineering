eda292733a532d78e9aad9a3c28520487ef90d35
NBoes <nicholas.2000@att.net>
2022-11-04 10:54:47 -0500

    Organization and Pugification

---
+++ public/script/paths.js
@@ -0,0 +1,22 @@
+let express = require('express');
+let appPTO = express()
+let port = 4000;
+let bodyParser = require('body-parser');
+
+appPTO.use(express.static('public'));
+appPTO.use(bodyParser.urlencoded({extended:true}));
+appPTO.set('view engine', 'pug');
+
+appPTO.get('/request', function (req, res) {
+    res.render('request');
+});
+
+appPTO.get('/login', function (req, res) {
+    res.render('login');
+});
+
+appPTO.get('/', function (req, res) {
+    res.render('employee');
+});
+
+appPTO.listen(port, () => console.log(`Listening on http://localhost:${port}/login`));
