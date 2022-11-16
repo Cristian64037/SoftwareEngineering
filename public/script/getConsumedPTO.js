8398d49593589f1eb612682458f1326821e3c61f
Cristian64037 <clara03@aurora.edu>
2022-11-16 04:27:14 -0600

    create query for consumed PTO

---
+++ public/script/getConsumedPTO.js
@@ -0,0 +1,16 @@
+const con= require("./databaseConnection");
+let results;
+const empID=774678
+con.connect(function(err) {
+    sql="select numofDays, PtoType.Pto_Name from Request  Join PtoType where PtoType.ptonameID=Request.ptonameID and empId=";
+    sql+=empID;
+    sql+=" AND StatNmeId=2 "
+    con.query(sql, function (err, result, fields) {
+        results=result;
+        //console.log(result);
+    });
+    //con.end();
+
+});
+
+module.exports = results;
