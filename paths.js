bb8d2e92b2c10a271c24cb872e5f261f561dedd5
Cristian64037 <clara03@aurora.edu>
2022-11-22 13:38:12 -0600

    update paths and employee views

--- paths.js
+++ paths.js
@@ -7,13 +7,66 @@ const Console = require("console");
 appPTO.use(express.static('public'));
 appPTO.use(bodyParser.urlencoded({extended:true}));
 appPTO.set('view engine', 'pug');
+appPTO.locals.moment = require('moment');
 
 appPTO.get('/team', function (req, res) {
     res.render('team');
 });
 
 appPTO.get('/supervisor', function (req, res) {
-    res.render('supervisor');
+    //Gets us all available balance
+    let PTO = require("./public/script/getPTOAvailable");
+    //This gets pending pto Requests
+    let PendingPTO=require("./public/script/getPendingPTOAvailable");
+
+    //Get us Max Values
+    let MaxPTO=require("./public/script/getMaxPTOValues");
+    
+    let Requests=require("./public/script/getRecuests");
+    
+
+    let User=require("./public/script/getUser");
+    
+
+    Promise.all([PTO,PendingPTO,MaxPTO,Requests,User]).then(function(data){
+        //TotalPTO
+        let pendingBalance=[0];
+        let consumedBalance=[0];
+        
+        consumedBalance[0] += parseInt(data[2][0].VacationTotal);
+        consumedBalance[0] -= parseInt(data[0][0].vbalance);
+        
+        
+
+
+        data[1].forEach(element => {
+            if (element.Pto_Name=="Vacation"){                
+                pendingBalance[0]+=parseInt(element.numofDays);
+                consumedBalance[0]-=parseInt(element.numofDays);
+            }
+        });
+        res.render('supervisor', {
+            data: data[3],
+            balanceData:data[0][0],
+            PendingPTORequest:pendingBalance,
+            consumedData: consumedBalance,
+            User :data[4][0]
+        });
+
+    });
+
+});
+
+appPTO.get('/history', function (req, res) {
+    res.render('history');
+});
+
+appPTO.get('/request', function (req, res) {
+    res.render('request');
+});
+
+appPTO.get('/login', function (req, res) {
+    res.render('login');
 });
 
 appPTO.get('/history', function (req, res) {
@@ -38,12 +91,33 @@ appPTO.get('/', function (req, res) {
     //Get us Max Values
     let MaxPTO=require("./public/script/getMaxPTOValues");
     let Requests=require("./public/script/getRecuests");
+    let User=require("./public/script/getUser");
     
 
-    Promise.all([PTO,PendingPTO,MaxPTO,Requests]).then(function(data){
+    Promise.all([PTO,PendingPTO,MaxPTO,Requests,User]).then(function(data){
         //Vacation,Personal,Sick
         let pendingBalance=[0,0,0];
         let consumedBalance=[0,0,0];
+
+        
+        // let PtoRequest=[]
+        // data[3].forEach(PtoRequest=>{
+        //     let ptoData={
+        //      "ptoRequestID":PtoRequest.ptoRequestID,
+        //      "Pto_Name":PtoRequest.ptoName,
+        //      "Day_Req":[PtoRequest.dateChanged],
+        //      "NmeOfStat":[PtoRequest.NmeOfStat],
+        //      "dateChanged":[PtoRequest.dateChanged],
+        //      "EmployeeChangedId":[PtoRequest.EmployeeChangedId],
+        //      "Comments":[PtoRequest.Comments]
+        //     }
+        //     PtoRequest.add(ptoData)
+             
+
+
+        // });
+        // console.log(PtoRequest);
+        
 
         consumedBalance[0] += parseInt(data[2][0].VacationTotal);
         consumedBalance[1] += parseInt(data[2][0].PersonalTotal);
@@ -74,7 +148,8 @@ appPTO.get('/', function (req, res) {
             data: data[3],
             balanceData:data[0][0],
             PendingPTORequest:pendingBalance,
-            consumedData: consumedBalance
+            consumedData: consumedBalance,
+            User :data[4][0]
         });
 
     });
