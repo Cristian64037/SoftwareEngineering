7720786cee0b6185fc24b6a918f958d12544af05
NBoes <nicholas.2000@att.net>
2022-12-05 20:29:28 -0600

    Working!!!

--- paths.js
+++ paths.js
@@ -10,8 +10,11 @@ const Pending = require("./public/script/getPendingPTOAvailable");
 const ConsumedPTO = require("./public/script/getMaxPTOValues");
 const UserRequests = require("./public/script/getRequests");
 const UserInfo = require("./public/script/getUser");
-let UserHistory = require("./public/script/getHistory");
-let user = require("./public/script/getAuthenticatedUser");
+const UserHistory = require("./public/script/getHistory");
+const user = require("./public/script/getAuthenticatedUser");
+const ReqPending = require("./public/script/getPendingRequest");
+const SuperEmployees = require("./public/script/getSupervisorEmployees");
+const con = require("./public/script/databaseConnection");
 
 //App uses
 appPTO.use(session({
@@ -24,15 +27,163 @@ appPTO.use(bodyParser.urlencoded({extended: true}));
 appPTO.set('view engine', 'pug');
 appPTO.locals.moment = require('moment');
 
+appPTO.post("/sendPtoRequest", function(req, res,){
+    let holidays = require("./public/script/getHolidays");
+
+    function isWeekend(date = new Date()) {
+        return date.getDay() === 6 || date.getDay() === 0;
+    }
+
+    function getDatesInRange(startDate, endDate) {
+        const date = new Date(startDate.getTime());
+        const dates = [];
+        while (date <= endDate) {
+            if(!(isWeekend((new Date(date))))){
+                dates.push(new Date(date));
+            }
+            date.setDate(date.getDate() + 1);
+        }
+        return dates;
+    }
+
+    let d1 = new Date(req.body.tripstart);
+    let d2 = new Date(req.body.tripend);
+    let dates2 = getDatesInRange(d1, d2);
+    let finalDatesList=[];
+
+    dates2.forEach(function(date){
+        finalDatesList.push(date.toDateString());
+
+    })
+
+    Promise.all([holidays]).then(function(data){
+        data[0].forEach(Dop => {
+            if (finalDatesList.includes((Dop.DayOff.toDateString()))) {
+                let index=dates2.indexOf(Dop.DayOff.toDateString());
+                finalDatesList.splice(index, 1);
+            } });
+        console.log(finalDatesList.length);
+
+        let InsertDate = new Date().toISOString();
+
+        let SqlPtoType="";
+
+        let PtoType=req.body.ptoType;
+
+        if(PtoType==3){
+            SqlPtoType="sbalance";
+        }else if(PtoType==1){
+            SqlPtoType="vbalance";
+        }else{
+            SqlPtoType="pbalance";
+        }
+
+        //Insert Request
+        con.query("INSERT INTO Request ( empID, ptonameID, StatNmeId, leaderID, numofDays, submitdate) VALUES ("+req.body.empID+
+            ","+req.body.ptoType+",1,"+req.body.leaderID+","+finalDatesList.length+",'"+InsertDate+"')", function(err, rows, fields) {
+            if (err) throw err;
+
+            //Get PtoRequestID
+            con.query("SELECT ptorequestID FROM `Request` WHERE  submitdate='"+InsertDate.slice(0,-5)+"'", function(err, result, fields) {
+                if (err) throw err;
+
+                let PtoId=result[0].ptorequestID;
+                //update in status
+                con.query("INSERT INTO PtoStatus(StatNmeId, ptorequestID, EmployeeId, EmployeeChangedId,"+
+                    " dateChanged , comments) VALUES (1,"+PtoId+","+req.body.empID+","+req.body.leaderID+",'"+InsertDate+"','"+req.body.Descript+"')", function(err, rows, fields) {
+                    if (err) throw err;
+
+                    //Insert Into Days Off
+                    con.query("INSERT INTO DayOff (ptorequestID, dayReq) VALUES ("+PtoId+",'"+req.body.tripstart+"')", function(err, rows, fields) {
+                        if (err) throw err;
+
+                        con.query("INSERT INTO DayOff (ptorequestID, dayReq) VALUES ("+PtoId+",'"+req.body.tripend+"')", function(err, rows, fields) {
+                            if (err) throw err;
+                            //Update in employee
+                            con.query("UPDATE Employee SET "+SqlPtoType+"="+SqlPtoType+"-"+finalDatesList.length+" WHERE empID="+req.body.empID, function(err, rows, fields) {
+                                if (err) throw err;
+                                res.redirect("/request");
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
+
+appPTO.post("/updateRequest", function (req, res,){
+    let managerId = req.session.username;
+    let SqlPtoType="";
+    let PtoType=req.body.ptoType;
+
+    if (PtoType=="Sick") {
+        SqlPtoType="sbalance";
+    } else if (PtoType=="Vacation") {
+        SqlPtoType="vbalance";
+    } else {
+        SqlPtoType="pbalance";
+    }
+
+    if (req.body.action === "approve") {
+        //update in request
+        con.query("UPDATE Request SET StatNmeId=2 WHERE ptorequestID="+req.body.requestID, function(err, rows, fields) {
+            if (err) throw err;
+            //update in status
+            con.query("INSERT INTO PtoStatus(StatNmeId, ptorequestID, EmployeeId, EmployeeChangedId,"+
+                " dateChanged , comments) VALUES (2,"+req.body.requestID+","+req.body.employeeID+","+managerId+",CURRENT_DATE() ,'"+req.body.requestComments+"')", function(err, rows, fields) {
+                if (err) throw err;
+                //Update Balance
+                res.redirect("/team");
+            });
+        });
+    } else if (req.body.action==="deny") {
+        //Update Employee Table
+        con.query("UPDATE Employee SET "+SqlPtoType+"="+SqlPtoType+"+"+parseInt(req.body.requestDays)+" WHERE empID="+req.body.employeeID, function(err, rows, fields) {
+            if (err) throw err;
+            //update in request
+            con.query("UPDATE Request SET StatNmeId=3 WHERE ptorequestID="+req.body.requestID, function(err, rows, fields) {
+                if (err) throw err;
+                //update in status
+                con.query("INSERT INTO PtoStatus(StatNmeId, ptorequestID, EmployeeId, EmployeeChangedId,"+
+                    " dateChanged , comments) VALUES (3,"+req.body.requestID+","+req.body.employeeID+","+managerId+",CURRENT_DATE() ,'"+req.body.requestComments+"')", function(err, rows, fields) {
+                    if (err) throw err;
+                    res.redirect("/team");
+                });
+            });
+        });
+    } else {
+        res.redirect("/team");
+    }
+});
+
 appPTO.get('/team', function (req, res) {
     //If the user is loggedin
     if (req.session.loggedin) {
         if (req.session.role !== 3) {
-            res.render('team', {
-                employee: {
-                    user: req.session.username,
-                    role: req.session.role
-                }
+            let PendingRequests = ReqPending.getPendingRequests(req);
+            let Employees = SuperEmployees.getSupervisorEmp(req);
+
+
+            Promise.all([PendingRequests, Employees]).then(function (data) {
+                let dict2 = {}
+
+                data[0].forEach(reqD => {
+                    if (!(reqD.ptorequestID in dict2)) {
+                        dict2[reqD.ptorequestID] = [[reqD.dayReq], reqD.Pto_Name, reqD.NmeOfStat, reqD.dateChanged, reqD.EmployeeChangedId, reqD.Comments, reqD.submitdate, reqD.empID, reqD.fname, reqD.lname, reqD.numofDays];
+                    } else {
+                        dict2[reqD.ptorequestID][0].push(reqD.dayReq)
+                    }
+                });
+
+                res.render('team', {
+                    pendingRequests: dict2,
+                    Employees: data[1],
+                    employee: {
+                        user: req.session.username,
+                        role: req.session.role
+                    }
+                });
             });
         } else {
             res.redirect("/");
@@ -107,11 +258,23 @@ appPTO.get('/supervisor', function (req, res) {
 appPTO.get('/request', function (req, res) {
     //If the user is loggedin
     if (req.session.loggedin) {
-        res.render('request', {
-            employee: {
-                user: req.session.username,
-                role: req.session.role
-            }
+        let User = UserInfo.getUser(req);
+
+        Promise.all([User]).then(function (data) {
+            // console.log(data[0])
+            ptoBalances = [0, 0, 0]
+            ptoBalances[0] += parseInt(data[0][0].pbalance);
+            ptoBalances[1] += parseInt(data[0][0].vbalance);
+            ptoBalances[2] += parseInt(data[0][0].sbalance);
+
+            res.render('request', {
+                User: data[0][0],
+                PtoBal: ptoBalances,
+                employee: {
+                    user: req.session.username,
+                    role: req.session.role
+                }
+            });
         });
     } else {
         // Not logged in
--- public/script/getSupervisorEmployees.js
+++ public/script/getSupervisorEmployees.js
@@ -1,26 +1,19 @@
-
 const con= require("./databaseConnection");
-//const empID=843864
-const empID=951666
 
-
-
-module.exports = new Promise (function (resolve, reject) {
-    // code to execute
-
-    con.connect(function (err) {
-        sql = "select * From Employee where Employee.leaderID=";
-        sql += empID;
-        sql += " "
-        // if (err) throw err;
-        con.query(sql, function (err, result, fields) {
-            //if (err) throw err;
-            resolve(result);
-            //dataN= resolve(result);
-
-            
+const getSupervisorEmp = (request) => {
+    const empID = request.session.username;
+    return new Promise(function (resolve, reject) {
+        con.connect(function (err) {
+            sql = "select * From Employee where Employee.leaderID=";
+            sql += empID;
+            sql += " "
+            // if (err) throw err;
+            con.query(sql, function (err, result, fields) {
+                if (err) throw err;
+                resolve(result);
+                //dataN= resolve(result);
+            });
         });
-
-
     });
-})
+}
+module.exports = {getSupervisorEmp}
